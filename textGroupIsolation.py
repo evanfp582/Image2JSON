ba5c9723f426eed9544134d75595cd6d159c6d28
Jacob Fricano <jsf7393@rit.edu>
2024-02-24 20:20:31 -0500

    Input to give each section meaning

--- textGroupIsolation.py
+++ textGroupIsolation.py
@@ -1,28 +1,64 @@
 import cv2
 import pytesseract
+import numpy as np
 
 
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+
+def getContours(img, kernel_size):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
+    dilated = cv2.dilate(thresh, kernel, iterations=4)
+    cv2.imshow("Dilated", cv2.resize(dilated, (0, 0), fx=0.2, fy=0.2))
+    cv2.waitKey(0)
+    contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    return contours
 
 
 def groupText(img):
     file = open("recognized.txt", "w+")
     file.write("")
     file.close()
+    height, width, _ = img.shape
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 30))
-    dilated = cv2.dilate(thresh, kernel, iterations=4)
-    cv2.imshow("Dilated", cv2.resize(dilated, (0, 0), fx=0.2, fy=0.2))
-    cv2.waitKey(0)
-    contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours = getContours(img, 30)
     file = open("recognized.txt", "a")
+    sections_num = 0
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
+        if abs(width-w) < 50 or w < 600:
+            continue
         rect = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cropped = img[y:y+h, x:x+w]
+        cropped = img[y:y + h, x:x + w]
+        inner_contours = getContours(cropped, 10)
+        sections = []
+        for cntr in inner_contours:
+            x1, y1, w1, h1 = cv2.boundingRect(cntr)
+            if w1 < 100:
+                continue
+            rect = cv2.rectangle(cropped, (x1, y1), (x1 + w1, y1 + h1), (255, 0, 0), 2)
+            sections.append([x1, y1])
 
+        sections.reverse()
+        for i in range(len(sections)):
+            x1, y1 = sections[i]
+            cv2.putText(cropped, str(i), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5, cv2.LINE_AA)
+
+        cv2.imshow("Cropped", cv2.resize(cropped, (0, 0), fx=0.3, fy=0.3))
+        key = cv2.waitKey(0)
+        if key == 27:
+            cv2.destroyAllWindows()
+
+        if sections_num != len(sections):
+            meanings = {}
+            sections_num = len(sections)
+            for i in range(len(sections)):
+                meaning = input(f"What is in section {i}?")
+                meanings[i] = meaning
+
+        print(meanings)
         text = pytesseract.image_to_string(cropped)
         if len(text) > 0:
             file.write(text)
