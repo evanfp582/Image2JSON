d5aa1a0a376b2a44dff8e0b71f2571b6b020b506
Jacob Fricano <jsf7393@rit.edu>
2024-02-24 23:39:06 -0500

    Pick what type to store text as

--- textGroupIsolation.py
+++ textGroupIsolation.py
@@ -1,6 +1,6 @@
 import cv2
 import pytesseract
-import numpy as np
+import json
 
 
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
@@ -11,8 +11,6 @@ def getContours(img, kernel_size):
     _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
     dilated = cv2.dilate(thresh, kernel, iterations=4)
-    cv2.imshow("Dilated", cv2.resize(dilated, (0, 0), fx=0.2, fy=0.2))
-    cv2.waitKey(0)
     contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     return contours
 
@@ -53,25 +51,46 @@ def groupText(img):
 
         if sections_num != len(sections):
             meanings = {}
+            types = {}
             sections_num = len(sections)
             for i in range(len(sections)):
                 meaning = input(f"What is in section {i}?")
+                store_type = input(f"How do you want to store the {meaning} section? ([string], array): ")
+                if store_type == "":
+                    store_type = "string"
+                while store_type != "string" and store_type != "array":
+                    store_type = input("Invalid Choice.\nHow do you want to store the text? ([string], array): ")
+                    if store_type == "":
+                        store_type = "string"
+
+                types[i] = store_type
                 meanings[i] = meaning
 
         parsed_info = {}
         for i in range(len(sections)):
             meaning = meanings.get(i)
+            store_type = types.get(i)
             x1, y1, w1, h1 = sections[i]
             area = img[y + y1:y + y1 + h1, x + x1:x + x1 + w1]
             text = pytesseract.image_to_string(area)
-            if meaning in parsed_info.keys():
-                parsed_info[meaning] = parsed_info.get(meaning) + text.replace("\n", "")
+            if store_type == "array":
+                parsed_info[meaning] = text.strip().split("\n")
+            elif meaning in parsed_info.keys():
+                text = text.strip().replace("\n", " ")\
+                    .replace("@", "1/2").replace("^", "2/3").replace("*", "1/3")\
+                    .replace("$", "1/4").replace("_", "3/4")
+                parsed_info[meaning] = parsed_info.get(meaning) + text
             else:
-                parsed_info[meaning] = text.replace("\n", " ")
+                text = text.strip().replace("\n", " ") \
+                    .replace("@", "1/2").replace("^", "2/3").replace("*", "1/3") \
+                    .replace("$", "1/4").replace("_", "3/4")
+                parsed_info[meaning] = text
 
         for sects in parsed_info.keys():
             print(sects + ":\n")
             print(parsed_info.get(sects))
+
+        print(json.dumps(parsed_info))
 
         text = pytesseract.image_to_string(cropped)
         if len(text) > 0:
@@ -79,6 +98,3 @@ def groupText(img):
             file.write("\n")
 
     file.close()
-
-    cv2.imshow("Image", cv2.resize(img, (0, 0), fx=0.3, fy=0.3))
-    cv2.waitKey(0)
